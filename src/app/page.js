8a6c15eeb47bf5dff3b15819393ff36251bb4cdc
eduardo1403 <eduardo1403hm@gmail.com>
2023-10-28 20:20:51 -0600

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,43 @@
+'use client'
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Image from 'next/image';
+
+function Categorias(){
+  const [categorias, setCategorias] = useState([]);
+
+  useEffect(()=>{
+    const ObtenerCategorias = async() =>{
+      const url = 'https://api-backend-mqv1.onrender.com/api/categories';
+      const result = await axios.get(url);
+
+      setCategorias(result.data);
+    }
+
+    ObtenerCategorias();
+  });
+
+
+  return(
+    <div>
+      <h1>Lista de datos ategorias ropa deportivas:</h1>
+      <ul>
+        {categorias.length === 0 && <p>Cargando....</p>}
+        {
+          categorias.map((datos, i) => {
+            return(
+              <li key={i}>
+                <h4>Nombre: {datos.name}</h4>
+                <p>Descripción: {datos.description}</p>
+                <p>Categoria: {datos.categorySex}</p>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default Categorias;
