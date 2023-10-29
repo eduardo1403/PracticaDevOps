b86b13d80aa1e1bb92aca88a12689535076db15c
eduardo1403 <eduardo1403hm@gmail.com>
2023-10-29 01:57:38 -0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Image from 'next/image';
 
 function Categorias(){
   const [categorias, setCategorias] = useState([]);
